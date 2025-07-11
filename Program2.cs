3e86d6d1b557a308c26424d8dce82aeb01456f2b
tiya946 <tiya090406@gmail.com>
2025-07-11 12:52:54 +0530

    change

---
+++ Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Lecture_BD
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            string name = "Pan Tiya";
+            string dob = "09/04/2006";
+            string address = "Tankara , Morbi";
+            string city = "Morbi";
+            int pin = 363650;
+            string state = "Gujrat";
+            string country = "India";
+            string email = "tiya090406@gmail.com";
+
+
+            Console.WriteLine("Name :" + name);
+            Console.WriteLine("DOB :" + dob);
+            Console.WriteLine("Address :" + address);
+            Console.WriteLine("City :" + city);
+            Console.WriteLine("Pin Code :" + pin);
+            Console.WriteLine("State :" + state);
+            Console.WriteLine("Country :" + country);
+            Console.WriteLine("Email :" + email);
+
+
+
+
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lecture_BD
+{
+    internal class Program3
+    {
+        public static void Main(string[] args)
+        {
+            int no = 23;
+            if(no % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
